2464300794ab9423a99edbaead67f8df2db59c26
peterstrongg <peterstrongdev@outlook.com>
2023-02-21 21:54:31 -0500

    added CodeSnipped component

--- src/lib/NotePage/NotePage.svelte
+++ src/lib/NotePage/NotePage.svelte
@@ -16,6 +16,7 @@
     />
 </div>
 
+
 <style>
     .notePage {
         width: 80rem;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import NotePage from "../lib/NotePage/NotePage.svelte";
+    import CodeSnippet from "../lib/CodeSnippet/CodeSnippet.svelte";
 </script>
 
 <h1>Synapse</h1>
 <NotePage />
+<CodeSnippet />
 
