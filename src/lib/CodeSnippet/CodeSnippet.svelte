dc86d9aeb4f9aef4290d2889e7b6fbf3f3f26a4e
peterstrongg <peterstrongdev@outlook.com>
2023-02-22 08:25:45 -0500

    minor changes

--- src/lib/CodeSnippet/CodeSnippet.svelte
+++
@@ -1,5 +0,0 @@
-<head>
-    <link href="../../prism.css" rel="stylesheet" />
-</head>
-<script src="../../prism.js"></script>
-<pre><code class="language-python">for i in range(len(10)):</code></pre>
