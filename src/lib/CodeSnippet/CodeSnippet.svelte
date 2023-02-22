6590d34807586fb751a68118b174fef09844526d
peterstrongg <peterstrong8343@gmail.com>
2023-02-22 11:44:27 -0500

    getting highlightjs to work

---
+++ src/lib/CodeSnippet/CodeSnippet.svelte
@@ -0,0 +1,13 @@
+
+<script lang="js">
+    import hljs from "highlight.js"
+    import javascript from 'highlight.js/lib/languages/javascript';
+    import 'highlight.js/styles/github.css';
+
+    hljs.registerLanguage("javascript", javascript)
+
+    const my_code = hljs.highlight("const x = 5;", {language: "javascript"}).value;
+</script>
+<div id="code-snippet">
+    {@html my_code}
+</div>
