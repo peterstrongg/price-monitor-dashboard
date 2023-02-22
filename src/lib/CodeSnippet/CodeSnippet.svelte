1a5a1d2344bae12797ebb464f0ef2772dafc445a
peterstrongg <peterstrongdev@outlook.com>
2023-02-21 23:45:51 -0500

    initial commit

---
+++ src/lib/CodeSnippet/CodeSnippet.svelte
@@ -0,0 +1,5 @@
+<head>
+    <link href="../../prism.css" rel="stylesheet" />
+</head>
+<script src="../../prism.js"></script>
+<pre><code class="language-python">for i in range(len(10)):</code></pre>
